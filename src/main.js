da08346bd7caba978d0baaefe0cb6743a549a743
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2023-03-20 13:38:23 +0100

    add local storage support

--- src/main.js
+++ src/main.js
@@ -4,12 +4,26 @@ import App from "./App.svelte";
 import { createEstimationStore } from "./lib/stores/estimations";
 import { createRouter } from "./lib/stores/router";
 
+const ESTIMATION_STORAGE_KEY = "svestimator:estimations";
+const DEFAULT_ESTIMATION_STORE_VALUE = [];
+
+const storageItem = localStorage.getItem(ESTIMATION_STORAGE_KEY);
+const storageValue = storageItem
+  ? JSON.parse(storageItem)
+  : DEFAULT_ESTIMATION_STORE_VALUE;
+
+const estimationStore = createEstimationStore(storageValue);
+
+estimationStore.subscribe((estimations) => {
+  localStorage.setItem(ESTIMATION_STORAGE_KEY, JSON.stringify(estimations));
+});
+
 const app = new App({
   target: document.getElementById("app"),
   // @ts-ignore
   context: new Map([
     ["router", createRouter()],
-    ["estimations", createEstimationStore([])],
+    ["estimations", estimationStore],
   ]),
 });
 
